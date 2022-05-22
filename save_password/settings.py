7b03d4744542310b54e12555abf546becc2dfd9c
AhteshamSid <78630600+AhteshamSid@users.noreply.github.com>
2022-05-22 11:33:25 +0530

    Update settings.py

--- save_password/settings.py
+++ save_password/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w_&41-=ry#!pa8##o4j%m40l48c1&542njk_b_4cdc9%yqnfj1'
+SECRET_KEY = 'Generate your own key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
